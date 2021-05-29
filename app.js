2eb7d1e189a0fd5e84b648b73451461118fc6af1
Aya Fawaz <84713762+ayazahra-oss@users.noreply.github.com>
2021-05-29 20:11:12 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+var userName ;
+var age = "21";
+
+var userName = prompt("Whats your name ?");
+alert("welcome to our websit " + userName );
+
+
+var ageinput = prompt("What is your fav animals ?");
+var favAnimals="cat";//console.log(userAge);
+var userAge= prompt("what your old ?");
+alert("welcom to our websit"+ userAge);
+
+if(favAnimals==
+ "cat" ){alert('yes it is not this');}else{alert('no it is not this')}
+document.write('<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fd9%2Fcd%2F69%2Fd9cd69b8fe5c67c052af138b04a6589d.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F832603049838012776%2F&tbnid=HWDoxfn2rqCj8M&vet=12ahUKEwiTuv-Ym-_wAhUXtaQKHZD2CC0QMygregUIARCTAg..i&docid=ZwPHdpkpBfA9rM&w=620&h=326&q=%D8%B5%D9%88%D8%B1%20%D9%82%D8%B7%D8%A9&safe=active&ved=2ahUKEwiTuv-Ym-_wAhUXtaQKHZD2CC0QMygregUIARCTAg"/>');
+if(ageinput == age ){
+  // console.log("what do you work ?");
+  alert('excatily');
+}else { alert('my old 21 years');
+}
+
+
+
+
+
